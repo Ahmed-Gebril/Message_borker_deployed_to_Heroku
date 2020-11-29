f7850bcc5f1a6b6e8a9a970d2b3de3aecbdd8fa2
Ahmed-Gebril <ahmed.gebril@student.tut.fi>
2020-11-29 12:47:57 +0200

    added state testing for full implmentation

--- APIGATEWAY/apigateway.py
+++ APIGATEWAY/apigateway.py
@@ -13,6 +13,36 @@ def get_messages():
 	response = requests.get('http://host.docker.internal:8080/')
 	return response.content,response.status_code,response.headers.items()
 
+def put_state():
+	payload = request.get_json() or request.form.to_dict()
+	#payload validation
+	if not payload:
+		return '',400
+
+	add_state(payload['state'])
+	return '',200
+
+@api.route('/api/state',methods=['GET'])
+def get_state():
+	current_state = get_current_state()
+
+	if current_state:
+		return get_current_state()['state'],200
+	else:
+		#returning empty response when there is no state presnet.
+		return '',200
+
+@api.route('/api/run-log',methods=['GET'])
+def get_run_log():
+	if not state_exists():
+		return '',200
+	data = get_redis_state()
+	formatted_data = [i['timestamp']+': '+i['state'] for i in data]
+	return '\n'.join(formatted_data)
+
+
+
+
 
 	
 if __name__ == "__main__":
--- tests/test_api.py
+++ tests/test_api.py
@@ -51,3 +51,21 @@ def test_get_run_log_endpoint(test_client):
     """
     response = test_client.get('/api/run-log')
     assert response.status_code == 200
+
+def test_get_state_endpoint(test_client):
+    """
+         Checks for a 200 status code when a GET request is send over '/api/state
+         Also checks for state to be INIT which is set by previous test case.
+    """
+    response = test_client.get('/api/state')
+    assert response.status_code == 200
+    #if test is run before starting orig, state should be INIT set by above `test_put_state_endpoint_with_payload`
+    #or state should be RUNNING if test is run after starting orig.
+    assert b'INIT' in response.data or b'RUNNING' in response.data
+
+def test_put_state_endpoint_without_payload(test_client):
+    """
+        Checks for a 401 status code when a PUT request is send over '/api/state without payload
+    """
+    response = test_client.put('/api/state')
+    assert response.status_code == 401
