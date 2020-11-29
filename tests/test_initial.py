de6a373a1db4bae898860450907172f2ac13df02
Ahmed-Gebril <ahmed.gebril@student.tut.fi>
2020-11-29 03:18:10 +0200

    improved documentation

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,5 +1,10 @@
 import requests
 
 def test_check_status_code_equals_200():
+
+      """
+        Checks for a 200 status code when a get request is sent to 
+        port 8080. Where messages are produced.
+      """
      response = requests.get("http://localhost:8080")
      assert response.status_code == 200
