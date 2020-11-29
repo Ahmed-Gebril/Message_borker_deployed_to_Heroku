0594ffc314f330a4eea98394f9816011cf90cddb
Ahmed-Gebril <ahmed.gebril@student.tut.fi>
2020-11-30 00:38:27 +0200

    fixing a bug in test implementation 'port 8081' should be used, and adding log to gitignore

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -5,6 +5,6 @@ def test_check_status_code_equals_200():
     """
        Checks for a 200 response when get request is sent to port 8080, where messages are produced.
     """
-    response = requests.get("http://172.19.0.1:8080")
+    response = requests.get("http://172.19.0.1:8081")
     assert response.status_code == 200
 
