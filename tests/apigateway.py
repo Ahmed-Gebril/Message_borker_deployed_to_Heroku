93e2225a7ffd6aa95da1b6e3f90c4680e06fb36b
Ahmed-Gebril <ahmed.gebril@student.tut.fi>
2020-11-29 11:32:29 +0200

    adding requirments to the tests

---
+++ tests/apigateway.py
@@ -0,0 +1,14 @@
+from flask import Flask,request,jsonify
+import os
+import requests
+import redis
+import json
+import datetime
+import time
+api = Flask(__name__)
+
+@api.route('/api/messages',methods=['GET'])
+def get_messages():
+	response = requests.get('http://172.19.0.1:8080/')
+	return response.content,response.status_code,response.headers.items()
+
