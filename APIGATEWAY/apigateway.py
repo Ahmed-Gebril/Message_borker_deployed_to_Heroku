6c869cc80c98bae1c1e5a37871ccec1b4bec6dcf
Ahmed-Gebril <ahmed.gebril@student.tut.fi>
2020-11-29 11:28:27 +0200

    adding apigateway api get messages endpoint

---
+++ APIGATEWAY/apigateway.py
@@ -0,0 +1,19 @@
+from flask import Flask,request,jsonify
+import os
+import requests
+import redis
+import json
+import datetime
+import time
+api = Flask(__name__)
+redis_client =  redis.Redis()
+
+@api.route('/api/messages',methods=['GET'])
+def get_messages():
+	response = requests.get('http://host.docker.internal:8080/')
+	return response.content,response.status_code,response.headers.items()
+
+
+	
+if __name__ == "__main__":
+	api.run()
