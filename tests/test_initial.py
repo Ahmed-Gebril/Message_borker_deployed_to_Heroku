d22e4ac03cd1de752fdaeb93b1a80da1fb60707d
Ahmed-Gebril <ahmed.gebril@student.tut.fi>
2020-11-29 03:25:16 +0200

    tests are going to be containerized, thefore we will choose gitlab docker gateway

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,10 +1,10 @@
 import requests
 
-def test_check_status_code_equals_200():
 
-      """
-        Checks for a 200 status code when a get request is sent to 
-        port 8080. Where messages are produced.
-      """
-     response = requests.get("http://localhost:8080")
-     assert response.status_code == 200
+def test_check_status_code_equals_200():
+    """
+       Checks for a 200 response when get request is sent to port 8080, where messages are produced.
+    """
+    response = requests.get("http://172.19.0.1:8080")
+    assert response.status_code == 200
+
