514ae6d498a621e4939ed5cfb9d9ded5f135aea4
Ahmed-Gebril <ahmed.gebril@student.tut.fi>
2020-11-30 11:55:10 +0200

    since the behavior of the applicaiton changed, the initial test is not valid anymore

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,10 +1,20 @@
 import requests
 
 
-def test_check_status_code_equals_200():
-    """
-       Checks for a 200 response when get request is sent to port 8080, where messages are produced.
-    """
-    response = requests.get("http://172.19.0.1:8081")
+""" def test_check_status_code_equals_200():
+  
+    response = requests.get("http://172.19.0.1:8080")
     assert response.status_code == 200
 
+ """
+
+
+ @api.route('/api/state',methods=['GET'])
+def get_state():
+	current_state = get_current_state()
+
+	if current_state:
+		return get_current_state()['state'],200
+	else:
+		#returning empty response when there is no state presnet.
+		return '',200
