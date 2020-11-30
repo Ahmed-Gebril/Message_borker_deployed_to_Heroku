979c3576f055b42ec289772b1e15cc2f26bf0fed
Ahmed-Gebril <ahmed.gebril@student.tut.fi>
2020-11-30 12:08:36 +0200

    fixing syntax error

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,20 +1,8 @@
 import requests
 
 
-""" def test_check_status_code_equals_200():
+def test_check_status_code_equals_200():
   
     response = requests.get("http://172.19.0.1:8080")
     assert response.status_code == 200
 
- """
-
-
- @api.route('/api/state',methods=['GET'])
-def get_state():
-	current_state = get_current_state()
-
-	if current_state:
-		return get_current_state()['state'],200
-	else:
-		#returning empty response when there is no state presnet.
-		return '',200
